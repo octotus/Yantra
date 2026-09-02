18937683591438a62dbddc2b45196f0733720436
octotus <karthi.sivaraman@gmail.com>
2026-09-01 20:15:19 -0400

    Fix finder Compose weight resolution

--- app/src/main/java/dev/yantra/app/ui/YantraFinderScreen.kt
+++ app/src/main/java/dev/yantra/app/ui/YantraFinderScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
